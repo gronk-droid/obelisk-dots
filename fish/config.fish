a0c0d5ddba4998cabc1de6377f63115cd5fdb428
gronk-droid <gronk.droid@gmail.com>
2025-07-08 13:43:20 -0400

    add asdf shims

--- fish/config.fish
+++ fish/config.fish
@@ -25,3 +25,17 @@ set tacklebox_path ~/.tackle ~/.tacklebox
 . ~/.tacklebox/tacklebox.fish
 
 fish_add_path /home/gronk-droid/.spicetify
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
